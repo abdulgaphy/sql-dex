8887ee5aba245f7c10c7e402e7b3573d2e1c59dd
silvareal <akubosylvernus@gmail.com>
2019-05-15 01:56:13 +0100

    whitenoise

--- projectwork/wsgi.py
+++ projectwork/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statesapi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projectwork.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
